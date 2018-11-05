9d5d5998b832c09db8d59ae9389d8c74fcd4d5d2
Nicacioneto <nicacionetobsb@hotmail.com>
2018-11-04 22:03:06 -0200

    Separete some code in functions and pass arguments from command line

--- candlestick_chart.py
+++ candlestick_chart.py
@@ -10,6 +10,38 @@ import pandas as pd
 import requests
 import sys
 
+def write_csv_header():
+    csv = open(sys.argv[1], 'a')
+    csv.write("t0,t1,t1_normalized,tweets_count,indicator\n")
+    csv.close()
+
+def write_csv_body(list_dict):
+    last_tweet_timestamp = ""
+    analyzer = SentimentIntensityAnalyzer()
+    csv = open(sys.argv[1], 'a')
+    for key in list_dict:
+        t0 = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
+            list_dict[key][0]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
+
+        t1 = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
+            list_dict[key][-1]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
+
+        t0_date_time = datetime.strptime(t0, '%Y-%m-%d %H:%M:%S')
+
+        t1_normalized = t0_date_time + timedelta(hours=1)
+
+        tweets_count = len(list_dict[key])
+        texts = []
+        for tweet in list_dict[key]:
+            texts.append(tweet["text"])
+
+        score = calc_sentiment(texts, analyzer)
+        indicator = score / tweets_count
+        csv.write(t0 + ", " + t1 + ", " + str(t1_normalized) + ", " + str(tweets_count) + ", " + str('%.3f' % indicator) + "\n")
+        last_tweet_timestamp = (t1_normalized - timedelta(hours=3)).timestamp()
+    csv.close()
+    return last_tweet_timestamp
+
 
 def get_ordered_dict(filename):
     list_dict = {}
@@ -39,9 +71,10 @@ def calc_sentiment(texts, analyzer):
 
 
 def build_graphs(timestamp):
+    print(timestamp)
     # Building Candlestick Graph
     plotly.tools.set_credentials_file(username='Nicacioneto', api_key='7K1twHAOzbFqTaYOwUU0')
-    url = "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=192&toTs=" + timestamp
+    url = "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=23&toTs=" + str(timestamp)
     response = requests.get(url)
     result = json.loads(response.text)['Data']
     for r in result:
@@ -58,7 +91,7 @@ def build_graphs(timestamp):
     # py.iplot(fig, filename='simple_candlestick')
 
     #Building Sentiment Graph
-    df = pd.read_csv("dataframe.csv")
+    df = pd.read_csv(sys.argv[1])
     trace2 = go.Bar(x=df.t1_normalized, y=df.indicator, xaxis='x2', yaxis='y2', marker=dict(
         color=make_color(df.indicator), colorscale='Viridis', showscale=True),)
 
@@ -76,7 +109,7 @@ def build_graphs(timestamp):
 
     data = [trace1, trace2]
     fig = go.Figure(data=data, layout=layout)
-    py.iplot(fig, filename='01-08-2018-07-08-2018')
+    py.iplot(fig, filename=sys.argv[1][0:15])
 
 
 def make_color(indicator_data):
@@ -87,39 +120,14 @@ def make_color(indicator_data):
 
 
 def main():
-    dataframe = open(sys.argv[1], 'a')
-    dataframe.write("'t0','t1','t1_normalized','tweets_count','indicator'")
-
-    filenames = [sys.argv[2], sys.argv[3]]
-    analyzer = SentimentIntensityAnalyzer()
-
-
+    write_csv_header()
+    filenames = [sys.argv[2]]
+    last_tweet_timestamp = ""
     for name in filenames:
-        dataframe = open(sys.argv[1], 'a')
-
         list_dict = get_ordered_dict(name)
-
-        for key in list_dict:
-            t0 = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
-                list_dict[key][0]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
-
-            t1 = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(
-                list_dict[key][-1]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
-
-            t0_date_time = datetime.strptime(t0, '%Y-%m-%d %H:%M:%S')
-
-            t1_normalized = t0_date_time + timedelta(hours=1)
-
-            tweets_count = len(list_dict[key])
-            texts = []
-            for tweet in list_dict[key]:
-                texts.append(tweet["text"])
-
-            score = calc_sentiment(texts, analyzer)
-            indicator = score / tweets_count
-            dataframe.write(t0 + ", " + t1 + ", " + str(t1_normalized) + ", " + str(tweets_count) + ", " + str('%.3f' % indicator) + "\n")
-        list_dict = {}
-        dataframe.close()
+        last_tweet_timestamp = write_csv_body(list_dict)
+        list_dict = []
+    build_graphs(last_tweet_timestamp)
 
 
 if __name__ == '__main__':
